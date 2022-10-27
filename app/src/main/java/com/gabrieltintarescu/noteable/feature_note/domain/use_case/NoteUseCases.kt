9f90be92f9a9fab641fac021ecf3bc127f27eba3
Gabriel Tintarescu <gabriel_tintarescu@yahoo.com>
2022-10-27 16:58:36 +0300

    ADD getNote to use cases wrapper

--- app/src/main/java/com/gabrieltintarescu/noteable/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/gabrieltintarescu/noteable/feature_note/domain/use_case/NoteUseCases.kt
@@ -8,5 +8,6 @@ package com.gabrieltintarescu.noteable.feature_note.domain.use_case
 data class NoteUseCases(
     val getNotes: GetNotes,
     val deleteNote: DeleteNote,
-    val addNote: AddNote
+    val addNote: AddNote,
+    val getNote: GetNote
 )
